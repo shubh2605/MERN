8f3171ce2fbb499a83db45a774172dd93e8f1b89
shubh2605 <singh10143shubham@gmail.com>
2026-07-06 10:49:44 +0530

    JavaScript Practice

---
+++ javaScript/checkEVen.js
@@ -0,0 +1,11 @@
+const age=15;
+const user="Shaswat";
+if(user=="Shubham")console.log("Welcome admin!");
+else console.log("Access denied:(");
+if(age >= 18){
+    console.log("Can vote");
+}else{
+    console.log("can't vote");
+}
+if(age%2==0)console.log("Even");
+else console.log("Odd");
---
+++ javaScript/script.js
@@ -0,0 +1,10 @@
+// var name='Shubham';
+// console.table({name:'Siuuuuuuuuuuuuuuuuu',age:41});
+// console.warn("HEllo");
+// alert('Hello');
+// console.group('Start');
+debugger;
+x = 30
+console.log(`Value of X is ${x}`);
+var x=10;
+console.log(`Value of x2 is ${x}`);
