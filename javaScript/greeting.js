81dad71dce0f321ca08d14d4345a9d365d3454ac
shubh2605 <singh10143shubham@gmail.com>
2026-07-08 13:59:13 +0530

    js Update

---
+++ javaScript/greeting.js
@@ -0,0 +1,26 @@
+// To      : {aarav.sharma@example.com}
+// Subject : Welcome to {JavaScript} Course
+
+// Hello Aarav Sharma,
+
+// We are happy to have you in our {JavaScript} course.
+// We hope you enjoy learning with us.
+// If you have any questions, feel free to contact us.
+
+// Phone : {9876543210}
+
+// Regards,
+// Parul University
+
+// 4. Return the highest examscore from students Array >>>
+
+// 5 Return all the student who are in BCA Course  >>> create a function that accepts CourseName and return result
+
+/*
+Welcome to Parul University  !
+
+We hope you have a wonderful learning experience in the ${student.course} program.
+
+Best Regards,
+Parul University `;
+*/
